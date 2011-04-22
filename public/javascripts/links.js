a87935f233e8ce2bdf8dea0df9bd0386a8b90603
Matt Mendell <matthew.mendell@gmail.com>
2011-04-22 13:49:17 -0700

    removed the now unnecessary jquery slider

--- public/javascripts/links.js
+++ public/javascripts/links.js
@@ -1,16 +1,4 @@
 $(function() {
-    $(".slider").each(function() {
-        var value = this.getAttribute("data-bias");
-        $(this).slider({
-            value: value,
-            min: 0,
-            max: 10,
-            slide: function(event, ui) {
-                $("#amount").val("$" + ui.value);
-            }
-        });
-    });
-    $("#amount").val("$" + $(".slider").slider("value"));
     $(".edit_link").submit(function() {
         $.post($(this).attr("action") + ".js", $(this).serialize(), null, "script");
         return false;
