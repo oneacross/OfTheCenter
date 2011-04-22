429464b72e0914202ce31f200081b22839f896e0
Matt Mendell <matthew.mendell@gmail.com>
2011-04-22 16:03:49 -0700

    all external links now open in a new tab

--- public/javascripts/links.js
+++ public/javascripts/links.js
@@ -3,4 +3,8 @@ $(function() {
         $.post($(this).attr("action") + ".js", $(this).serialize(), null, "script");
         return false;
     });
+    // open external links in new tab
+    $('a[rel="external"]').click(function() {
+        $(this).attr('target', '_blank');
+    });
 });
