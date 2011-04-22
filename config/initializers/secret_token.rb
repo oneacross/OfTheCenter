f43f537e6eaecaa7455264d4cde1297575d22e61
Matt Mendell <matthew.mendell@gmail.com>
2011-04-22 06:34:36 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ofthecenter::Application.config.secret_token = 'c104d743e48328de47624f8a31eee580ebe434287ef90bd7b243c07ce0d72f9053f08b2b14f45f2dacad9cfe19950877f75cf504e88d0917afca8b25f681db19'
