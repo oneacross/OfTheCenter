122399504316e5dbbffba453a8a5735accbf5977
Matt Mendell <matthew.mendell@gmail.com>
2011-04-22 09:53:31 -0700

    sliders now initialize to their bias value!

--- public/javascripts/links.js
+++ public/javascripts/links.js
@@ -1,11 +1,14 @@
 $(function() {
-    $(".slider").slider({
-        value: 0,
-        min: -10,
-        max: 10,
-        slide: function(event, ui) {
-            $("#amount").val("$" + ui.value);
-        }
+    $(".slider").each(function() {
+        var value = this.getAttribute("data-bias");
+        $(this).slider({
+            value: value,
+            min: -5,
+            max: 5,
+            slide: function(event, ui) {
+                $("#amount").val("$" + ui.value);
+            }
+        });
     });
     $("#amount").val("$" + $(".slider").slider("value"));
 });
