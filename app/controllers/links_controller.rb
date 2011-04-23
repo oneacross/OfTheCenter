ccc1632c119133797f825b22dadda655886dec3a
Matt Mendell <matthew.mendell@gmail.com>
2011-04-23 15:54:56 -0700

    after creating a link, redirect to root

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -66,7 +66,7 @@ class LinksController < ApplicationController
 
     respond_to do |format|
       if @link.save
-        format.html { redirect_to(@link, :notice => 'Link was successfully created.') }
+        format.html { redirect_to(root_url, :notice => 'Link was successfully added.') }
         format.xml  { render :xml => @link, :status => :created, :location => @link }
       else
         format.html { render :action => "new" }
